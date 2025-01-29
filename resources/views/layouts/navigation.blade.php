d039b26c1490930edbc10a70a53e5a7684be6a89
aidil <aidil.ikbar@kpk.go.id>
2025-01-29 13:57:12 +0100

    update links

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -16,12 +16,17 @@
                     <x-nav-link :href="route('homepage')" :active="request()->routeIs('homepage')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
-                </div>
 
-                <!-- Decision Support -->
-                <x-nav-link :href="route('decision-support.index')" :active="request()->routeIs('decision-support.*')">
+                    <!-- Decision Support -->
+                    <x-nav-link :href="route('decision-support.index')" :active="request()->routeIs('decision-support.*')">
                         {{ __('Decision Support') }}
                     </x-nav-link>
+
+                    <!-- Swagger Documentation -->
+                    <a class="inline-flex items-center px-1 pt-1 border-b-2 border-transparent text-sm font-medium leading-5 text-gray-500 dark:text-gray-400 hover:text-gray-700 dark:hover:text-gray-300 hover:border-gray-300 dark:hover:border-gray-700 focus:outline-none focus:text-gray-700 dark:focus:text-gray-300 focus:border-gray-300 dark:focus:border-gray-700 transition duration-150 ease-in-out" href="{{ url('/api/documentation') }}" target="_blank">
+                        Swagger Documentation
+                    </a>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
